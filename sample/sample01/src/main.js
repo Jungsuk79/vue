8c4776de6415b9c6f280257b8658394add2afc56
Jungsuk79 <ppackjung@gmail.com>
2025-05-09 00:20:02 +0900

    111

---
+++ sample/sample01/src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
